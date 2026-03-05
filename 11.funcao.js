fc587962531fe81142b365d2c55a85f2dea41ab6
madu teixeira <m.eduardateixeirasilva@outlook.com>
2026-03-05 16:45:56 -0300

    funções

---
+++ 11.funcao.js
@@ -0,0 +1,16 @@
+// Função comum com js
+
+function sum(a,b) {
+    return a + b
+}
+
+function sub(a,b) {
+    return a - b
+    
+}
+
+const summ = sum(2,3)
+const subb = sub(2,3)
+
+console.log("Sum: ",summ)
+console.log("Sub: ",subb)
