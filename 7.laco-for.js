10f5a0b3f9eea085d07f5341eafdf75c7ba1a1c8
madu teixeira <m.eduardateixeirasilva@outlook.com>
2026-03-05 15:07:49 -0300

    laco for

---
+++ 7.laco-for.js
@@ -0,0 +1,21 @@
+// Resumo : começa em 1
+// enquanto for <= 5
+// vai somando +1
+// e roda o código
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+// Laço for = for (COMEÇO; CONDIÇÃO; PASSO)
+
+// (let i = 1) = cria uma variável de 'i' com valor 1 
+
+// i <= 5 = enquanto i for menor ou igual a 5
+// ou seja: 
+// 1 <= 5 
+// 3 <= 5 
+// 4 <= 5 
+// 5 <= 5 
+// 6 <= 5  (para aqui)
+
+// i++ = i+1 
+// i+= 2 (seria para pular de 2 em 2)
