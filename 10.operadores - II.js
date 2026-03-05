26bddb80ed5ef84997a8d85d54da5f8b72c5f410
madu teixeira <m.eduardateixeirasilva@outlook.com>
2026-03-05 16:25:30 -0300

    operadores lógicos

---
+++ 10.operadores - II.js
@@ -0,0 +1,12 @@
+// Operadores lógicos :
+ condicao_a = 1 < 2
+ condicao_b = 10 < 2
+
+ console.log("- And -")
+ console.log(condicao_a && condicao_b)
+
+ console.log("\n- Not -")
+ console.log(condicao_a && !condicao_b)
+
+ console.log("\n- Or -")
+ console.log(condicao_a  || condicao_b)
