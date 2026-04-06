69481159e9d2b70de0d6430b8380810b1ecffb65
Aluno <Aluno@C302-LAB-000.senaiba.com.br>
2026-04-06 16:08:49 -0300

    pag com css

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AdicionarUser from './components/AdicionarUser'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div className='app'>
+        <Header />
+        <main>
+          <AdicionarUser />
+        </main>
+        <Footer />
+      </div>
       
     </>
   )
